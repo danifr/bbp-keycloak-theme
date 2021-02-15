a6a000917a9a25a55befe1c8f3fdc92404c75a21
Daniel Fernández <daferoes@gmail.com>
2021-02-15 10:58:16 +0100

    Adapt login theme to Blue Brain Project

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@ set -o errexit
 declare -r currentDir="$(dirname "${BASH_SOURCE[0]}")"
 source "${currentDir}/build.properties"
 
-DISTRIBUTION_NAME=alfresco-keycloak-theme-$THEME_VERSION
+DISTRIBUTION_NAME=bbp-keycloak-theme-$THEME_VERSION
 
 # prepare and zip the theme content
 echo "info::: Removing an existing '$DISTRIBUTION_NAME.zip' file."
 rm -rf $DISTRIBUTION_NAME.zip
 
-mkdir alfresco
-echo "info::: Packaging alfresco theme as '$DISTRIBUTION_NAME.zip'"
+mkdir bbp
+echo "info::: Packaging bbp theme as '$DISTRIBUTION_NAME.zip'"
 
-cp -rf theme/* alfresco/
-zip -r $DISTRIBUTION_NAME.zip alfresco
+cp -rf theme/* bbp/
+zip -r $DISTRIBUTION_NAME.zip bbp
 
 echo "info::: Cleanup temp files and folders."
-rm -rf alfresco
+rm -rf bbp
--- theme/login/login.ftl
+++ theme/login/login.ftl
@@ -20,11 +20,11 @@
         </script>
     <#elseif section = "form">
         <div>
-            <img class="logo" src="${url.resourcesPath}/img/alfresco-logo.svg" alt="Alfresco">
+            <img class="logo" src="${url.resourcesPath}/img/bbp-logo.svg" alt="Blue Brain Project - EPFL">
         </div>
         <div class="box-container">
             <div>
-                <p class="application-name">Alfresco Identity Service</p>
+                <p class="application-name">Log in with your GASPAR account</p>
             </div>
         <#if realm.password>
             <div>
